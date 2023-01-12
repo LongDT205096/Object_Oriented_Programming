fa19c323180549b9f04ccdb88e618fee67b7fc9b
LongDT205096 <dinhthanhlong0@gmail.com>
2023-01-12 16:31:44 +0700

    Initial commit

--- AimsProject/src/hust/soict/dsai/aims/screen/Interface/controller/CartScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/Interface/controller/CartScreen.java
@@ -22,6 +22,7 @@ public class CartScreen extends JFrame {
         JFXPanel fxPanel = new JFXPanel();
         this.add(fxPanel);
         this.setTitle("Cart");
+        this.setSize(1280, 768);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         
